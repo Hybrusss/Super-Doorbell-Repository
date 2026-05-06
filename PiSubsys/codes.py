4dacb6765b1938792cb6a466a98e8af55a913a92
Hybrusss <t.ryan.coolidge@gmail.com>
2026-05-06 10:49:39 -0500

    forgot these changes last night

--- PiSubsys/codes.py
+++ PiSubsys/codes.py
@@ -41,7 +41,7 @@ class code:
 			
 		fileHandle.close()
 
-	def createListFromFile(file:str) -> list[code]:
+	def createListFromFile(file:str) -> list:
 		# The files for the doorbell program will be stored in the home folder
 		fileHandle = open(f"{Path.home()}/doorbell/settings/" + file, "br")
 		contents = fileHandle.read()
@@ -78,7 +78,3 @@ class code:
 
 		fileHandle.close()
 		return
-
-# Tests
-code.createFileFromList("testFile.set", [code(1234, "file.ogg"), code(4321, "honk.ogg")])
-code.createListFromFile("testFile.set")
--- PiSubsys/piMain.py
+++ PiSubsys/piMain.py
@@ -1,111 +1,133 @@
 from webBackend import *
+import codeAPI
+import playsound
 
 pdThr = None
-resetTimer = None
+mutex = threading.Lock()
 
-PIN_COL0 = 0
-PIN_COL1 = 0
-PIN_COL2 = 0
-PIN_ROW0 = 0
-PIN_ROW1 = 0
-PIN_ROW2 = 0
-PIN_ROW3 = 0
+PIN_COL0 = 18
+PIN_COL1 = 19
+PIN_COL2 = 20
+PIN_ROW0 = 17
+PIN_ROW1 = 16
+PIN_ROW2 = 13
+PIN_ROW3 = 12
+WAIT_TIME = 0.055
 
 currentColumn = -1
 currentPinStr = ""
+lastPressTime = 0
 
+def _buttonHandler(channel:int):
+    with mutex:
+        global currentPinStr
+        global lastPressTime
+        
+        if time.time() - lastPressTime > 5:
+            currentPinStr = ""
+        
+        if channel == PIN_ROW0: currentRow = 0
+        elif channel == PIN_ROW1: currentRow = 1
+        elif channel == PIN_ROW2: currentRow = 2
+        elif channel == PIN_ROW3: currentRow = 3
+        curBut = ''
+        if currentColumn == 0:
+            if currentRow == 0:
+                if time.time() - lastPressTime < .100:
+                    curBut = ''
+                else:
+                    curBut = '1'
+            elif currentRow == 1:
+                curBut = '4'
+            elif currentRow == 2:
+                if time.time() - lastPressTime < .100:
+                    currentPinStr = currentPinStr[:-1]
+                curBut = '7'
+            elif currentRow == 3:
+                curBut = '*'
+        elif currentColumn == 1:
+            if currentRow == 0:
+                if time.time() - lastPressTime < .100:
+                    currentPinStr = currentPinStr[:-1]
+                curBut = '2'
+            elif currentRow == 1:
+                curBut = '5'
+            elif currentRow == 2:
+                curBut = '8'
+            elif currentRow == 3:
+                curBut = '0'
+        elif currentColumn == 2:
+            if currentRow == 0:
+                curBut = '3'
+            elif currentRow == 1:
+                curBut = '6'
+            elif currentRow == 2:
+                if time.time() - lastPressTime < .100:
+                    curBut = ''
+                else:
+                    curBut = '9'
+            elif currentRow == 3:
+                curBut = '#'
+        if curBut == '':
+            return
 
-def _resetPinStr():
-	currentPinStr = ""
-
-def _buttonHandler(currentRow:int):
-	curBut = ''
-	if currentColumn == 0:
-		if currentRow == 0:
-			curBut = '1'
-		elif currentRow == 1:
-			curBut = '4'
-		elif currentRow == 2:
-			curBut = '7'
-		elif currentRow == 3:
-			curBut = '*'
-	if currentColumn == 1:
-		if currentRow == 0:
-			curBut = '2'
-		elif currentRow == 1:
-			curBut = '5'
-		elif currentRow == 2:
-			curBut = '8'
-		elif currentRow == 3:
-			curBut = '0'
-	if currentColumn == 2:
-		if currentRow == 0:
-			curBut = '3'
-		elif currentRow == 1:
-			curBut = '6'
-		elif currentRow == 2:
-			curBut = '9'
-		elif currentRow == 3:
-			curBut = '#'
-	if curBut == '':
-		return
-	global resetTimer
-	if resetTimer == None:
-		resetTimer = threading.Timer(5, _resetPinStr)
-	resetTimer.cancel()
-
-	global currentPinStr
-	currentPinStr += curBut
-
-	resetTimer.run()
-	pass
+        currentPinStr += curBut
+        if currentPinStr[-1] == '#':
+            sndFile = codeAPI.retrieveSound(int(currentPinStr[:-1]))
+            try:
+                playsound.playsound(sndFile)
+            finally:
+                currentPinStr = ""
+        
+        lastPressTime = time.time()
 
 def _pinpadDriverThread():
-	import RPi.GPIO as GPIO
+    import RPi.GPIO as GPIO
 
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(PIN_COL0, GPIO.OUT)
-	GPIO.setup(PIN_COL1, GPIO.OUT)
-	GPIO.setup(PIN_COL2, GPIO.OUT)
-	GPIO.setup(PIN_ROW0, GPIO.IN, GPIO.PUD_DOWN)
-	GPIO.setup(PIN_ROW1, GPIO.IN, GPIO.PUD_DOWN)
-	GPIO.setup(PIN_ROW2, GPIO.IN, GPIO.PUD_DOWN)
-	GPIO.setup(PIN_ROW3, GPIO.IN, GPIO.PUD_DOWN)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(PIN_COL0, GPIO.OUT)
+    GPIO.setup(PIN_COL1, GPIO.OUT)
+    GPIO.setup(PIN_COL2, GPIO.OUT)
+    GPIO.setup(PIN_ROW0, GPIO.IN, GPIO.PUD_DOWN)
+    GPIO.setup(PIN_ROW1, GPIO.IN, GPIO.PUD_DOWN)
+    GPIO.setup(PIN_ROW2, GPIO.IN, GPIO.PUD_DOWN)
+    GPIO.setup(PIN_ROW3, GPIO.IN, GPIO.PUD_DOWN)
 
-	GPIO.add_event_detect(PIN_ROW0, GPIO.RISING, _buttonHandler, 1)
-	GPIO.add_event_detect(PIN_ROW1, GPIO.RISING, _buttonHandler, 1)
-	GPIO.add_event_detect(PIN_ROW2, GPIO.RISING, _buttonHandler, 1)
-	GPIO.add_event_detect(PIN_ROW3, GPIO.RISING, _buttonHandler, 1)
+    GPIO.add_event_detect(PIN_ROW0, GPIO.RISING, _buttonHandler, 1)
+    GPIO.add_event_detect(PIN_ROW1, GPIO.RISING, _buttonHandler, 1)
+    GPIO.add_event_detect(PIN_ROW2, GPIO.RISING, _buttonHandler, 1)
+    GPIO.add_event_detect(PIN_ROW3, GPIO.RISING, _buttonHandler, 1)
+    global currentColumn
+    try:
+        # The following loop iterates through each column and row to see which button is pressed
+        # This orientation does not support multiple buttons and will
+        while True:
+            currentColumn = 0
+            GPIO.output(PIN_COL0, GPIO.HIGH) # Set column 0 pin to HIGH
+            with mutex:
+                GPIO.output(PIN_COL0, GPIO.LOW) # Reset column pin
+            time.sleep(WAIT_TIME)
 
-	try:
-		# The following loop iterates through each column and row to see which button is pressed
-		# This orientation does not support multiple buttons and will
-		while True:
-			currentColumn = 0
-			GPIO.output(PIN_COL0, GPIO.HIGH) # Set column 0 pin to HIGH
-			time.sleep(0.001) # Wait for 1 millisecond for the hardware to react
-			GPIO.output(PIN_COL0, GPIO.LOW) # Reset column pin
-			time.sleep(0.001)
+            currentColumn = 1
+            GPIO.output(PIN_COL1, GPIO.HIGH) # Set column 1 pin to HIGH
+            with mutex:
+                GPIO.output(PIN_COL1, GPIO.LOW) # Reset column pin
+            time.sleep(WAIT_TIME)
 
-			currentColumn = 1
-			GPIO.output(PIN_COL1, GPIO.HIGH) # Set column 1 pin to HIGH
-			time.sleep(0.001) # Wait for 1 millisecond for the hardware to react
-			GPIO.output(PIN_COL1, GPIO.LOW) # Reset column pin
-			time.sleep(0.001)
-
-			currentColumn = 2
-			GPIO.output(PIN_COL2, GPIO.HIGH) # Set column 2 pin to HIGH
-			time.sleep(0.001) # Wait for 1 millisecond for the hardware to react
-			GPIO.output(PIN_COL2, GPIO.LOW) # Reset column pin
-			time.sleep(0.001)
-		
-	finally:
-		webRunning = False
-		GPIO.cleanup()
-		print("Exiting doorbell subsystem...")
-		exit(0)
+            currentColumn = 2
+            GPIO.output(PIN_COL2, GPIO.HIGH) # Set column 2 pin to HIGH
+            with mutex:
+                GPIO.output(PIN_COL2, GPIO.LOW) # Reset column pin
+            time.sleep(WAIT_TIME)
+        
+    finally:
+        webRunning = False
+        GPIO.cleanup()
+        print("Exiting doorbell subsystem...")
+        exit(0)
 
 
 createUDPBroadcaster()
 createRuntimeServer()
 _pinpadDriverThread()
+
